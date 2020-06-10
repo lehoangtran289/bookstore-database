41394e58e0da1c20a6df559013dd371c1ab12614
SonTH123 <haison250199@gmail.com>
2020-06-10 22:22:18 +0700

    Modify the trigger

--- BookStoreTrigger.sql
+++ BookStoreTrigger.sql
@@ -1,30 +1,27 @@
 -- Update the number of book in table book when books are bought
-DELIMETER $$
-CREATE TRIGGER update_book_quantity ON order_detail AFTER INSERT AS
-BEGIN
+DELIMITER $
+
+CREATE TRIGGER update_book_quantity BEFORE INSERT ON order_detail
+	FOR EACH ROW 
 	UPDATE book 
-	SET inventory_qty = inventory_qty - (
-		SELECT quantity
-		FROM order_detail
-		WHERE book_id = book.book_id
-	)
-	FROM book
-	JOIN order_detail ON book.book_id = order_detail.book_id;
-END
-DELIMETER ;
+	SET inventory_qty = inventory_qty - NEW.quantity
+	WHERE book_id = NEW.book_id;
+$
+
+DELIMITER ;
 
 -- Update table orders to get the total_bill of an order when books are added into order
-DELIMETER $$
-CREATE TRIGGER order_total_bill ON order_detail AFTER INSERT AS
-BEGIN
+DELIMITER $
+
+CREATE TRIGGER order_total_bill BEFORE INSERT ON order_detail
+	FOR EACH ROW
 	UPDATE orders
 	SET total_bill = total_bill + (
-		SELECT SUM(b.price * od.quantity)
-		FROM order_detail AS od, book AS b
-		WHERE od.order_id = orders.order_id
-		AND b.book_id = od.book_id
+		SELECT (price * NEW.quantity)
+		FROM book
+		WHERE book_id = NEW.book_id
 	)
-	FROM orders 
-	JOIN order_detail ON orders.order_id = order_detail.order_id;
-END
-DELIMETER ;
+	WHERE order_id = NEW.order_id;
+$
+
+DELIMITER ;
